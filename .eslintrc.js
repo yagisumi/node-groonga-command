afd0d8a8a9d7a2e66f0dbb21458b0c68bf416d58
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-05-04 11:21:36 +0900

    Update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
+    // 'eslint:recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/eslint-recommended',
   ],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {},
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
   env: { browser: true, node: true, es6: true },
   globals: {},
   rules: {
