9905e53cd0215cc3bf8cde3f20cf407a181ab6c9
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-05-07 13:31:06 +0900

    Add test

--- test/groonga-command.test.ts
+++ test/groonga-command.test.ts
@@ -1,5 +1,12 @@
 import { GroongaCommand, TypeGuards } from '@/groonga-command'
 
 describe('GroongaCommand', () => {
-  test('GroongaCommand', () => {})
+  test('GroongaCommand', () => {
+    const command = new GroongaCommand('select', { table: 'Users' })
+    expect(command.command_name).toBe('select')
+    expect(TypeGuards.isSelect(command)).toBe(true)
+    if (TypeGuards.isSelect(command)) {
+      expect(command.table).toBe('Users')
+    }
+  })
 })
