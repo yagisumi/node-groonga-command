5b5f3fbec2cc54d93106e53c8fd7c9176adf1043
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-05-07 11:40:47 +0900

    Add export

--- src/groonga-command.ts
+++ src/groonga-command.ts
@@ -1,3 +1,4 @@
-export { GroongaCommand, createCommand } from './command/groonga_command'
 export { TypeGuards } from './type_guards'
+export { GroongaCommand, createCommand } from './command/groonga_command'
 export { Drilldown, Slice, FormatOptions } from './types'
+export { parseCommand } from './parse_command'
